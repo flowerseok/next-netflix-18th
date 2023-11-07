f1fc0b61cb0388177c3cc7f714b779000cb282f1
Jiseok <chae0_@naver.com>
2023-11-08 00:33:23 +0900

    feat:API call test

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
-
-const Browse = () => {
-  return <div>Browse</div>;
+'use client';
+import React,{ useEffect, useState } from 'react';
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+type Movie = {
+  id: number;
+  title: string;
+  poster_path: string;
+  overview: string;
 };
 
-export default Browse;
+export default function Browse() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+        const json = await response.json();
+        console.log(json);
+        setMovies(json.results);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    }
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.title}</h4>
+          <img src= {'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt = {movie.title} />
+          <p>{movie.overview}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+
+
+// const Browse = () => {
+//   return <div>Browse</div>;
+// };
+
+// export default Browse;

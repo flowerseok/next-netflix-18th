4609c1f2c7d3421b3a7d1f42a72d144ebcdaa15c
Jiseok <chae0_@naver.com>
2023-11-08 20:48:23 +0900

    feat:useAPI

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,34 +1,18 @@
-import React from 'react';
+import { getMovies } from '@/utils/Api';
 
-type Movie = {
-  id: number;
-  title: string;
-  poster_path: string;
-  overview: string;
+async function Page() {
+  const movies = await getMovies('popular');
+  console.log(movies);
+  return movies;
+}
+
+const Browse = () => {
+  const Content=Page();
+  return (
+    <div className="bg-black w-full h-full">
+      <div></div>
+    </div>
+  );
 };
 
-async function getMovies(type: string) {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${type}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`
-  );
-  return res.json();
-}
-
-export default async function Page() {
-  const popularMovies = await getMovies('popular');
-  const movies = popularMovies.results;
-  return (
-    <>
-      {movies.map((movie: Movie) => (
-        <div key={movie.id}>
-          <h4>{movie.title}</h4>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-          />
-          <p>{movie.overview}</p>
-        </div>
-      ))}
-    </>
-  );
-}
+export default Browse;
